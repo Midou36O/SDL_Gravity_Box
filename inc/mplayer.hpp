3a2349d517d6ae38493ea48c2b80e3f49a0567e6
Midou36O <facemioo4@gmail.com>
2023-04-18 00:41:34 +0000

    Initial work on the Music player (broken, won't compile.)

---
+++ inc/mplayer.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+
+
+class MusicPlayer {
+    public:
+        loadMusic(const char* p_filepath);
+        void playMusic();
+        void initMixer();
+        void initMusic();
+
+    private:
+        Mix_Music* music;
+};
---
+++ src/mplayer.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file mplayer.cpp
+ * @author Midou (midou@midou.dev)
+ * @brief Simple music player.
+ * @version 0.1
+ * @date 2023-04-17
+ * 
+ * 
+ */
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <iostream>
+
+#include "mplayer.hpp"
+
+
+MusicPlayer::loadMusic(const char* p_filepath) {
+
+    Mix_Music* music = NULL;
+    music = Mix_LoadMUS(p_filepath);
+
+    if (music == NULL) {
+        std::cerr << "Failed to load music! ERROR: " << Mix_GetError() << std::endl;
+        return 1;
+    }
+    Mix_PlayMusic(music, -1);
+    return music;
+}
+
+void MusicPlayer::playMusic() {
+    if (Mix_PlayingMusic() == 0) {
+        std::cerr << "Music is not playing!" << std::endl;
+    }
+}
+
+void MusicPlayer::initMixer() {
+	Mix_Init(MIX_INIT_MP3);
+	SDL_Init(SDL_INIT_AUDIO);
+	if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+		std::cerr << "SDL_Mixer couldnt init. Err: " <<  Mix_GetError() << std::endl;
+	}
+}
